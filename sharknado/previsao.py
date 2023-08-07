76cad3c9525a41c130da453223bcebd2d4caa393
Costa <klinsmann.m.costa@accenture.com>
2023-08-07 17:58:56 -0300

    Updates files

--- sharknado/previsao.py
+++ sharknado/previsao.py
@@ -1,30 +1,38 @@
 import requests
-from dotenv import dotenv_values
+from decouple import config
 
-#link do open_weather: https://openweathermap.org/
+# link do open_weather: https://openweathermap.org/
 
-api_secret = dotenv_values('.env')
-#input('Informe o nome da cidade: ')
-nome_da_cidade = 'recife'
+api_secret = config('API_KEY')
 
-link = f'https://api.openweathermap.org/data/2.5/weather?q={nome_da_cidade}&appid={api_secret["API_KEY"]}&lang=pt_br'
+# input('Informe o nome da cidade: ')
 
-requisicao = requests.get(link, verify=False)
 
-if requisicao.status_code != 200:
-    print('Não foi possível obter a localização.')
+def requisita_tempo(nome_da_cidade):
 
-else:
-    requisicao_dic = requisicao.json()
-    descricao = requisicao_dic['weather'][0]['description']
-    temperatura = int(requisicao_dic['main']['temp'] - 273.15)
-    temperatura_min = int(requisicao_dic['main']['temp_min'] - 273.15)
-    temperatura_max = int(requisicao_dic['main']['temp_max'] - 273.15)
-    #print(requisicao_dic)
-    print(requisicao_dic['sys']['country'])
-    print(str(requisicao_dic['coord']['lon']) + ' ' + str(requisicao_dic['coord']['lat']))
-    print(requisicao_dic['name'])
-    print(requisicao_dic['weather'][0]['description'])
-    print('Temperatura ' + str(temperatura) + 'º')
-    print('Temperatura mínima ' + str(temperatura_min) + 'º')
-    print('Temperatura máxima ' + str(temperatura_max) + 'º')
+    link = f'https://api.openweathermap.org/data/2.5/weather?q={nome_da_cidade}&appid={api_secret}&lang=pt_br'
+
+    requisicao = requests.get(link, verify=False)
+
+    if requisicao.status_code != 200:
+        requisicao_dic = 'Não foi possível obter a localização.'
+        return requisicao_dic
+    else:
+        requisicao_dic = requisicao.json()
+        return requisicao_dic
+
+
+# print(requisita_tempo('sa paulo'))
+
+# descricao = requisicao_dic['weather'][0]['description']
+# temperatura = int(requisicao_dic['main']['temp'] - 273.15)
+# temperatura_min = int(requisicao_dic['main']['temp_min'] - 273.15)
+# temperatura_max = int(requisicao_dic['main']['temp_max'] - 273.15)
+# #print(requisicao_dic)
+# print(requisicao_dic['sys']['country'])
+# print(str(requisicao_dic['coord']['lon']) + ' ' + str(requisicao_dic['coord']['lat']))
+# print(requisicao_dic['name'])
+# print(requisicao_dic['weather'][0]['description'])
+# print('Temperatura ' + str(temperatura) + 'º')
+# print('Temperatura mínima ' + str(temperatura_min) + 'º')
+# print('Temperatura máxima ' + str(temperatura_max) + 'º')
--- tests/test_previsao.py
+++ tests/test_previsao.py
@@ -1,5 +1,9 @@
-from sharknado import previsao
+from sharknado.previsao import requisita_tempo
 
 
-def test_previsao():
-    ...
+def test_previsao_true():
+    print(requisita_tempo('recife'))
+
+
+def test_previsao_false():
+    print(requisita_tempo('sa paulo'))
