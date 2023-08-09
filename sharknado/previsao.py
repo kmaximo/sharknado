8260cf2cffb4bf24480c784b1e271c40a55d5b8e
Costa <klinsmann.m.costa@accenture.com>
2023-08-09 17:04:08 -0300

    Update files

--- sharknado/previsao.py
+++ sharknado/previsao.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
+from datetime import datetime
 from decouple import config
+from pathlib import Path
 
 # link do open_weather: https://openweathermap.org/
 
@@ -8,6 +10,10 @@ api_secret = config('API_KEY')
 
 
 def requisita_tempo(nome_da_cidade):
+    
+    print("-----------------------------")
+    print("-Começando extração de dados-")
+    print("-----------------------------")
 
     link = f'https://api.openweathermap.org/data/2.5/weather?q={nome_da_cidade}&appid={api_secret}&lang=pt_br'
 
@@ -18,18 +24,23 @@ def requisita_tempo(nome_da_cidade):
         return requisicao_dic
     else:
         requisicao_dic = requisicao.json()
-        return {
+        dados_cidade = [{
+            "pais": requisicao_dic['sys']['country'],
+            "cidade": requisicao_dic["name"],
             "temperatura": int(requisicao_dic["main"]["temp"] - 273.15),
             "temperatura_min": int(requisicao_dic["main"]["temp_min"] - 273.15),
             "temperatura_max": int(requisicao_dic["main"]["temp_max"] - 273.15),
             "descricao": requisicao_dic["weather"][0]["description"],
             "icon": requisicao_dic["weather"][0]["icon"],
-            "pais": requisicao_dic['sys']['country'],
-            "cidade": requisicao_dic["name"]     
-        }
+            "data_requisicao": datetime.now().strftime("%y%m%d")     
+        }]
+        cidade_df = pd.DataFrame(dados_cidade)
+        cidade_df.to_csv('sharknado/hist/cities_weather_req.csv', mode='a', index='', header=False)
+        
+         
 
 
-print(requisita_tempo('recife'))
+requisita_tempo('recife')
 
 # descricao = requisicao_dic['weather'][0]['description']
 # temperatura = int(requisicao_dic['main']['temp'] - 273.15)
